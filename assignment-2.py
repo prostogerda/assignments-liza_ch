3db16aed2e8cb5af4ccf93051ca9f55d8dfb0a23
prostogerda <elizabet92@list.ru>
2016-02-05 17:47:53 +0300

    .com

--- assignment-2.py
+++ assignment-2.py
@@ -1,4 +1,24 @@
 #! /usr/bin/env python
 
+
 from __future__ import division, print_function
 
+
+def my_max(*args):
+    for maxim in args:
+        pass
+    # In this case it's possible to use   maxim = None
+    # instead of two previous lines (first iteration)
+    for index in args:
+        if index > maxim:
+            maxim = index
+    return maxim
+
+
+def my_min(*args):
+    for minim in args:
+        pass
+    for index in args:
+        if index < minim:
+            minim = index
+    return minim
