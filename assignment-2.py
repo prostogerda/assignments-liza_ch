e2a35f2bb88c548dedfd959812863e5e8186b96c
prostogerda <elizabet92@list.ru>
2016-02-07 18:39:57 +0300

    .com

--- assignment-2.py
+++ assignment-2.py
@@ -11,6 +11,7 @@ def my_max(*args):
     my_max(*args) finds maximum in some values,
     list or tuple of integer numbers
     """
+    iterated_list = args
     if not args:
         raise ValueError("Please enter some numbers")
     if len(args) == 1:
@@ -18,9 +19,8 @@ def my_max(*args):
         if not isinstance(iterated_list, (tuple, list)):
             raise ValueError("There is only one value {}"
                              ", and it isn't tuple or list".format(args[0]))
-        args = iterated_list
-    current_max = args[0]
-    for num in args:
+    current_max = iterated_list[0]
+    for num in iterated_list:
         if not isinstance(num, int):
             raise ValueError("Input value {}"
                              " is not integer".format(num))
@@ -50,3 +50,6 @@ def my_min(*args):
         if num < current_min:
             current_min = num
     return current_min
+
+
+def my_
