23f931277f7ce8a414287822b9d8c5cac402a1ad
prostogerda <elizabet92@list.ru>
2016-02-07 16:22:30 +0300

    .com

--- assignment-2.py
+++ assignment-2.py
@@ -4,83 +4,98 @@
 from __future__ import division, print_function
 
 
-# Не уверена, что это оптимальный вариант, но он, по крайней мере,
-# уже почти выполняет все условия (пока не получилось с одним аргументом).
-
-
-"""
-my_max(*args) finds current_maxum value in values, list or tuple
-"""
+# Мне тут Женя говорит, что ты ему сказал, что my_max(одно число) должно
+#  выдавать это число. А в задании написано, что должен выдавать ошибку,
+# что это не список. Написала пока для второго варианта. Над вложением
+# функции в функцию пока думаю :)
 
 
 def my_max(*args):
+    """
+    my_max(*args) finds maximum in some values,
+    list or tuple of integer numbers
+    """
+    if not args:
+        raise ValueError("Please enter some numbers")
+    if len(args) == 1:
+        for iterated_list in args:
+                pass
+        if not isinstance(iterated_list, (tuple, list)):
+            raise ValueError("There is only one value {}"
+                             ", and it isn't tuple or list".format(*args))
+        current_max = iterated_list[0]
+        for num in iterated_list:
+            if not isinstance(num,int):
+                raise ValueError("Input value {}"
+                                 " is not integer".format(num))
+            if num > current_max:
+                current_max = num
     if len(args) > 1:
         current_max = args[0]
-        for numb in args:
-            if numb.__class__ == int and numb > current_max:
-                current_max = numb
-            elif numb.__class__ != int:
+        for num in args:
+            if not isinstance(num, int):
                 raise ValueError("Input value {}"
-                                 " is not integer".format(numb))
-    elif len(args) == 1:
-        if args.__class__ == tuple or list:
-            for iteratedList in args:
-                pass
-            current_max = iteratedList[0]
-            for numb in iteratedList:
-                if numb.__class__ == int and numb > current_max:
-                    current_max = numb
-                elif numb.__class__ != int:
-                    raise ValueError("Input value {}"
-                                     " is not integer".format(numb))
-    else:
-        raise ValueError("There is only one value {}"
-                         "and it isn't tuple or list".format(*args))
+                                 " is not integer".format(num))
+            if num > current_max:
+                current_max = num
     return current_max
 
 
 def my_min(*args):
+    """
+    my_min(*args) finds minimum in some values,
+    list or tuple of integer numbers
+    """
+    if not args:
+        raise ValueError("Please enter some numbers")
+    if len(args) == 1:
+        for iterated_list in args:
+                pass
+        if not isinstance(iterated_list, (tuple, list)):
+            raise ValueError("There is only one value {}"
+                             ", and it isn't tuple or list".format(*args))
+        current_min = iterated_list[0]
+        for num in iterated_list:
+            if not isinstance(num,int):
+                raise ValueError("Input value {}"
+                                 " is not integer".format(num))
+            if num < current_min:
+                current_max = num
     if len(args) > 1:
         current_min = args[0]
-        for numb in args:
-            if numb.__class__ == int and numb < current_min:
-                current_min = numb
-            elif numb.__class__ != int:
+        for num in args:
+            if not isinstance(num, int):
                 raise ValueError("Input value {}"
-                                 " is not integer".format(numb))
-    elif len(args) == 1:
-        if args.__class__ == tuple or list:
-            for iteratedList in args:
-                pass
-            current_min = iteratedList[0]
-            for numb in iteratedList:
-                if numb.__class__ == int and numb < current_min:
-                    current_min = numb
-                elif numb.__class__ != int:
-                    raise ValueError("Input value {}"
-                                     " is not integer".format(numb))
-    else:
-        raise ValueError("There is only one value {}"
-                         "and it isn't tuple or list".format(*args))
+                                 " is not integer".format(num))
+            if num < current_min:
+                current_min = num
     return current_min
 
 
 def test(*args):
-    def in_test(args):
-        for current_max in args:
-            pass
-        for numb in args:
-            if numb.__class__ == int and numb > current_max:
-                current_max = numb
-            elif numb.__class__ != int:
+    """
+    my_max(*args) finds maximum of values,
+    list or tuple of integer numbers
+    """
+    def inner_func(*inner_args):
+        current_max = inner_args[0]
+        for num in inner_args:
+            if not isinstance(num, int):
                 raise ValueError("Input value {}"
-                                 " is not integer".format(numb))
+                                 " is not integer".format(num))
+            if num > current_max:
+                current_max = num
         return current_max
-
+    if not args:
+        raise ValueError("Please enter some numbers")
+    if len(args) == 1:
+        for iterated_list in args:
+                pass
+        if not isinstance(iterated_list, (tuple, list)):
+            raise ValueError("There is only one value {}"
+                             ", and it isn't tuple or list".format(*args))
+        maximum = inner_func(iterated_list)
     if len(args) > 1:
-        in_test(args)
-    elif len(args) == 1 and args.__class__ == tuple or list:
-        for iteratedList in args:
-            pass
-        in_test(iteratedList)
-    return in_test(args)
+        maximum = inner_func(args)
+    return maximum
+
