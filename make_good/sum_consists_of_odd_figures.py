b3bf68be9a5aac5c5f8d95f17a5fe241b9556478
prostogerda <elizabet92@list.ru>
2016-07-03 14:40:57 +0300

    Code to improve, part 2. Something is incorrect in "mum_of_odd_sums" function

---
+++ make_good/sum_consists_of_odd_figures.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python3
+
+
+from functools import reduce
+
+
+def reverse(num_str: str) -> int:
+    """
+    Function reverses the number (abcd -> dcba)
+    :param num_str:
+    :return:
+    """
+    return int(num_str[::-1])
+
+
+def digits_are_odd(num: int) -> bool:
+    """
+    Function checks odd confirmation for every digit
+    :param num:
+    :return:
+    """
+    return all(int(figure) % 2 for figure in str(num))
+
+
+def num_of_odd_sums(numbers: list):
+    """
+    Counts number of cases, that match the condition:
+    sum of number and "reversed" number consists of odd figures
+    :param numbers:
+    :return:
+    """
+    return reduce(sum, (1 if digits_are_odd(int(num_str) + reverse(num_str))
+                        else 0 for num_str in numbers))
+    # something is wrong in this function
+
+
+def main():
+    n = int(input("Insert number of cases: "))
+    numbers = [input("Print number: ") for _ in range(n)]
+    print(num_of_odd_sums(numbers))
+
+
+if __name__ == '__main__':
+    main()
+
+
+
